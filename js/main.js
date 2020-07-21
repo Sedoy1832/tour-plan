23a87ff114b63480120851baba49194cb029bd8e
Demin Yuriy <xkayx@bk.ru>
2020-07-21 19:47:47 +0300

    create: add ymap and adaptive

--- js/main.js
+++ js/main.js
@@ -11,3 +11,43 @@ var mySwiper = new Swiper(".swiper-container", {
     onlyInViewport: false,
   },
 });
+ymaps.ready(init);
+
+function init() {
+  var myMap = new ymaps.Map(
+      "map",
+      {
+        center: [48.15266, 11.598704],
+        zoom: 15,
+      },
+      {
+        searchControlProvider: "yandex#search",
+      }
+    ),
+    // Создаем геообъект с типом геометрии "Точка".
+    myGeoObject = new ymaps.GeoObject({
+      // Описание геометрии.
+      geometry: {
+        type: "Point",
+        coordinates: [48.15266, 11.598704],
+      },
+      properties: {
+        // Контент метки.
+        iconContent: "Я",
+        hintContent: "Hilton Munich Park",
+      },
+    });
+  myMap.geoObjects
+    .add(myGeoObject)
+    .add(myPieChart)
+    .add(
+      new ymaps.Placemark(
+        [48.15266, 11.598704],
+        {},
+        {
+          preset: "islands#icon",
+          iconColor: "#FF2400",
+        }
+      )
+    );
+}
