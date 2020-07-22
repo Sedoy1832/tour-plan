fa2a1c2e9b129cbdbf0b18862df2aa00a5210b5c
Demin Yuriy <xkayx@bk.ru>
2020-07-22 19:02:50 +0300

    Create: add section newsletter

--- js/main.js
+++ js/main.js
@@ -15,36 +15,31 @@ ymaps.ready(init);
 
 function init() {
   var myMap = new ymaps.Map(
-      "map",
-      {
-        center: [48.15266, 11.598704],
-        zoom: 15,
-      },
-      {
-        searchControlProvider: "yandex#search",
-      }
-    ),
-    // Создаем геообъект с типом геометрии "Точка".
-    myGeoObject = new ymaps.GeoObject({
-      // Описание геометрии.
-      geometry: {
-        type: "Point",
-        coordinates: [48.15266, 11.598704],
-      },
-      properties: {
-        // Контент метки.
-        iconContent: "Я",
-        hintContent: "Hilton Munich Park",
-      },
-    });
-  myMap.geoObjects.add(myGeoObject).add(
-    new ymaps.Placemark(
-      [48.15266, 11.598704],
-      {},
-      {
-        preset: "islands#icon",
-        iconColor: "#FF2400",
-      }
-    )
+    "map",
+    {
+      center: [48.15266, 11.598704],
+      zoom: 15,
+    },
+    {
+      searchControlProvider: "yandex#search",
+    }
   );
+
+  myPlacemark1 = new ymaps.Placemark(
+    [48.15266, 11.598704],
+    {
+      // Свойства.
+      // Содержимое иконки, балуна и хинта.
+      iconContent: "Я",
+      balloonContent: "Hilton Munich Park",
+      hintContent: "Hilton Munich Park",
+    },
+    {
+      // Опции.
+      // Стандартная фиолетовая иконка.
+      preset: "twirl#redIcon",
+    }
+  );
+  // Добавляем все метки на карту.
+  myMap.geoObjects.add(myPlacemark1);
 }
