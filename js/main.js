1b01d2c1cdb804a5071e92b96f7d227c6a5fdfa9
Demin Yuriy <xkayx@bk.ru>
2020-08-03 14:39:06 +0300

    fix: validation

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
         },
         phone: {
           required: "Телефон обязателен",
+          minlength: "Телефон должен содержать 11 цифр",
         },
       },
     });
