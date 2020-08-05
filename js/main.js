353dd0d1a0b15f40f4ba542d40582305fc3c087c
Demin Yuriy <xkayx@bk.ru>
2020-08-05 19:12:26 +0300

    fix: load

--- js/main.js
+++ js/main.js
@@ -35,38 +35,7 @@ $(document).ready(function () {
       onlyInViewport: false,
     },
   });
-  ymaps.ready(init);
 
-  function init() {
-    var myMap = new ymaps.Map(
-      "map",
-      {
-        center: [48.15266, 11.598704],
-        zoom: 15,
-      },
-      {
-        searchControlProvider: "yandex#search",
-      }
-    );
-
-    myPlacemark1 = new ymaps.Placemark(
-      [48.15266, 11.598704],
-      {
-        // Свойства.
-        // Содержимое иконки, балуна и хинта.
-        balloonContent: "Hilton Munich Park",
-        hintContent: "Hilton Munich Park",
-      },
-      {
-        // Опции.
-        preset: "twirl#redIcon",
-      }
-    );
-    // Добавляем все метки на карту.
-    myMap.geoObjects.add(myPlacemark1);
-    // отключаем Scroll
-    myMap.behaviors.disable("scrollZoom");
-  }
   $(".newsletter").parallax({ imageSrc: "img/newsleter-bg.jpg" });
 
   var responseSlider = new Swiper(".response-slider", {
