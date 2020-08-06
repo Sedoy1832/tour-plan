a75f74184fe2820686a2a16ea3563719d83d44dc
Demin Yuriy <xkayx@bk.ru>
2020-08-06 16:08:48 +0300

    create: block packages and fix load

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+  document.querySelector("#map").addEventListener("mouseenter", () => {
+    var d1 = document.getElementById("map");
+    if ($("#map__google").length == 0) {
+      d1.insertAdjacentHTML(
+        "afterbegin",
+        '<iframe id="map__google" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.6061410598268!2d79.84287911534405!3d6.937586094987594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae25920c53a5e23%3A0x56efc3724db4f567!2sGrand%20Oriental%20Hotel!5e0!3m2!1sru!2sru!4v1596635378267!5m2!1sru!2sru"></iframe>'
+      );
+    }
+  });
+
   // Обработка форм
   $(".form").each(function () {
     $(this).validate({
@@ -35,8 +45,6 @@ $(document).ready(function () {
       onlyInViewport: false,
     },
   });
-
-  $(".newsletter").parallax({ imageSrc: "img/newsleter-bg.jpg" });
 
   var responseSlider = new Swiper(".response-slider", {
     // Optional parameters
